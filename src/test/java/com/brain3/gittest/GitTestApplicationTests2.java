991049e0310eeb2324bc94963382b95daac0f0a0
hongchen.zhang@mytogo.com <hongchen.zhang@mytogo.com>
2018-08-21 09:18:32 +0800

    b-003

---
+++ src/test/java/com/brain3/gittest/GitTestApplicationTests2.java
@@ -0,0 +1,17 @@
+package com.brain3.gittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitTestApplicationTests2 {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("git test!");
+	}
+
+}
